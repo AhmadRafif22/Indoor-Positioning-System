1a99d5472aba212f9ffcbfb42ab1cb82f0fe016b
AhmadRafif22 <rafifayuzaki123@gmail.com>
2024-05-19 08:22:19 +0700

    fix-halaman-pengguna

--- website IPS/resources/views/pengguna/pengguna-home.blade.php
+++ website IPS/resources/views/pengguna/pengguna-home.blade.php
@@ -121,7 +121,7 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
+            <footer class="sticky-footer">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
                         <span>Copyright &copy; IPS 2024</span>
